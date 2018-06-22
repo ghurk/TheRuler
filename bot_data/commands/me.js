b510fdc2cfb6b0d50a080b42b085ba2e69850c72
ghurk <32410305+ghurk@users.noreply.github.com>
2018-06-22 14:51:42 +0200

    Update me.js

--- bot_data/commands/me.js
+++ bot_data/commands/me.js
@@ -1,5 +1,5 @@
 
-exports.command = 'meow';
+exports.command = 'me';
 exports.description = `i am able to tell you details about yourself\nFormat:\n'${global.prefix}me'`;
 exports.func = function( message ) {
 	message.delete().catch(O_o=>{});
