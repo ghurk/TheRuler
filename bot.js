00983cc6be729f647e806b293af5b7a63dfd0ce8
ghurk <32410305+ghurk@users.noreply.github.com>
2018-06-27 15:09:56 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,96 +9,13 @@ const config = require("./bot_data/config.json");
 global.prefix = config.prefix;
 
 
-
-//load commands from folder
-var commands = [];
-const fileSystem = require('fs');
-fileSystem.readdirSync('./bot_data/commands/').forEach(file => {
-  console.log(file);
-  commands.push( require('./bot_data/commands/'+file) );
-})
-
-function messageControl(message) {
-
-  if ( message.author.bot ) return; //return if author is bot
-
-  //message.author.send('you sent message to chat didnt ya');
-  if ( message.channel.type === 'dm' ) return; //personal message
-
-  if ( !message.content.startsWith(global.prefix) ) return; //return if message doesnt start with prefix
-
-  if ( message.content.toLowerCase().startsWith( global.prefix+'info' ) ) {
-
-    var sufix = message.content.toLowerCase().match(/info (.*)/);
-    if ( sufix !== null ) sufix = sufix[1];
-    console.log(sufix);
-
-    //print general info if no sufix is specified
-    if ( sufix === null ) {
-      message.delete().catch(O_o=>{});
-      message.channel.send(`\`\`\`prolog\nuse command '${global.prefix}info commands' to get list of available commands\nuse command '${global.prefix}info {command}' to get details about specified command\`\`\``);
-      return;
-    }
-    //print list of commands on 'commands' sufix
-    else if ( sufix === 'commands' ) {
-      var string = `\`\`\`prolog\nlist of available commands:\n`;
-      commands.forEach(function(command) {
-        string += `'${global.prefix}${command.command}'\n`;
-      });
-      message.delete().catch(O_o=>{});
-      message.channel.send(`${string}\`\`\``);
-      return;
-    }
-    //else look for command in list if other sufix is specified
-    else {
-      var found = false;
-      commands.forEach(function(command) {
-        if ( command.command === sufix ) {
-          message.delete().catch(O_o=>{});
-          message.channel.send(`\`\`\`prolog\n'${global.prefix}${command.command}' info:\n${command.description}\`\`\``);
-          found = true;
-          return;
-        }
-      });
-      if ( found === false ) {
-        message.delete().catch(O_o=>{});
-        message.channel.send(`\`\`\`prolog\ncommand '${global.prefix}${sufix}' not found\`\`\``);
-        return;
-      }
-    }
-  }
-  else {
-
-    var found = false;
-
-    //check if message command matches any of loaded commands
-    commands.forEach(function(command) {
-      if ( message.content.toLowerCase().startsWith( config.prefix+command.command ) ) {
-        //execute command and return
-        command.func(message);
-        found = true;
-        return;
-      }
-    });
-
-    if ( found === false ) {
-      message.channel.send(`\`\`\`prolog\ncommand not recognized\`\`\``);
-    }
-
-  }
-}
-
-
-
-
-
     
     
     
 //client control object
 var clients = [];
 var tokens = [];
-//tokens.push(config.token2);
+tokens.push(config.token2);
 tokens.push(config.token3);
 
 //initialize a bot per token
@@ -126,7 +43,6 @@ clients.forEach( function(client,index) {
       play();
     });
   }
-  
 
   client.on("ready", () => {
     console.log(`Bot ${index} Activated`);
@@ -134,14 +50,31 @@ clients.forEach( function(client,index) {
   });
   client.on("message", async message => {
     if ( message.content.toLowerCase().startsWith( global.prefix+'player'+index ) ) {
+      //also add permissions check
       
-      if ( message.member.voiceChannel !== undefined ) {
-        message.member.voiceChannel.join() //join users voiceChannel
-        .then( connection => {
-          client.connection = connection;
-          play();
-        })
-        .catch( console.error );
+      //--player play
+      if ( message.content.toLowerCase().startsWith(global.prefix+'player'+index+" play") ) {
+        if ( message.member.voiceChannel !== undefined ) {
+          message.member.voiceChannel.join() //join users voiceChannel
+          .then( connection => {
+            client.connection = connection;
+            play();
+          })
+          .catch( console.error );
+        }
+      }
+      //--player add ...
+      else if ( message.content.toLowerCase().startsWith(global.prefix+'player'+index+" add") ) {
+        //regex find anything after add {url}
+        //load url data, on success add url to playlist array in format playlist.push( {url:'...',name:'...',length:'...'} );
+      }
+      //--player remove ...
+      else if ( message.content.toLowerCase().startsWith(global.prefix+'player'+index+" remove") ) {
+        //regex find anything after remove {index/all}
+        //if result = all => playlist.clear(); else playlist.remove(index)
+      }
+      else if ( message.content.toLowerCase().startsWith(global.prefix+'player'+index+" end") ) {
+        //player leave room and stop stream
       }
       
     }
